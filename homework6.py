4c492a4c98add615fce1c025a878ea9e19debcc7
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-25 06:37:41 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,29 @@
+#my_dict = {"Elena" : 1972 , "Olga" : 1985 , "Maxim" : 1999}
+#print(my_dict)
+
+#print(my_dict["Olga"])
+
+#print(my_dict.get("Sergey"))
+
+#my_dict["Efim"] = 2000
+#my_dict["Vika"] = 2010
+
+#del my_dict["Olga"]
+#print(my_dict)
+
+my_set = {19, 25, 46, "port", "word" , "port" ,  25, 11, 19, 25}
+my_set.add(44)
+list = [37,"true"]
+#print(my_set.update(list)) # почему не правильно? не могу понять((
+my_set.update([37, "True ,"])
+print(my_set)
+
+my_set.discard(11)
+print(my_set)
+
+
+
+
+
+
+
